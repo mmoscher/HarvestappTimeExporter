8c40ee5a8fd4d2fdc00d09bbb8c8e656141f3701
MoschersMarco <M.Moscher@gmx.de>
2013-11-20 20:23:24 +0100

    Update Readme / move config.secret.php.sample

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 #HarvestappT Time Exporter
-===============================================
+------------------------------------------------
 
-A simple Webapp to offer you the possibility to provide a simple access on your current timetracks for your clients.
+A simple WebApp to offer you the possibility to provide a simple access on your current timetracks for your clients.
 
-####Technolgies
+### Installation
+
+Just grab a copy on a server and edit the /lib/config.secret.php.sample and save the file as /lib/config.secret.php
+That's all!
+**Have fun =)**
+
+#### Technolgies
+
 HTML5Boilerplate
 Bootstrap v3.0
 [HaPi](http://mdbitz.com/harvest-api/) (thanks @ _Matthew J. Denton_ for this great PHP API)
--- index.php
+++ index.php
@@ -45,55 +45,17 @@ if( !$clientResult->isSuccess() ) {
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     </head>
     <body>
-        <!--[if lt IE 7]>
-            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-        <![endif]-->
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-          <form class="navbar-form navbar-right">
-            <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
-          </form>
-        </div><!--/.navbar-collapse -->
-      </div>
-    </div>
+
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
         <h1>Hello, world!</h1>
-        <p>Now lets see what you did there!</p>
+        <p>Now lets see what you tracked there =)!</p>
       </div>
     </div>
 
